d26f5e2788fddc3400c29287d04086c848e2d879
Nicolas Goudry <goudry.nicolas@gmail.com>
2021-03-31 14:19:45 +0200

    chore: update release git commit message

--- release.config.js
+++ release.config.js
@@ -42,7 +42,7 @@ module.exports = {
     '@semantic-release/npm',
     ['@semantic-release/git', {
       assets: ['package.json'],
-      message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+      message: 'chore(release): v${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
     }],
   ],
 }
